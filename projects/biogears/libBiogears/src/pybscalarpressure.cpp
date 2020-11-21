a1ea6186ab5407c578266e9b6fc418ec801353e7
Vybhav <vybhav@hipo.ai>
2020-11-21 13:44:05 +0530

    Some more shit working

--- projects/biogears/libBiogears/src/pybscalarpressure.cpp
+++ projects/biogears/libBiogears/src/pybscalarpressure.cpp
@@ -1,4 +1,6 @@
 #include "../include/biogears/cdm/properties/SEScalarPressure.h"
+#include "../include/biogears/cdm/properties/SEScalarQuantity.h"
+#include "../include/biogears/cdm/utils/unitconversion/CompoundUnit.h"
 
 #include <pybind11/pybind11.h>
 
@@ -7,16 +9,14 @@ namespace py = pybind11;
 
 
 
+PYBIND11_MODULE(pybscalarpressure, m) {
 
-PYBIND11_MODULE(pybscalarpresssure, m) {
+//    py::class_<biogears::PressureUnit,biogears::CCompoundUnit>(m, "PressureUnit");
 
-    py::class_<biogears::SEScalarPressure>(m, "SEScalarPressure")
-    .def(py::init<>())
+//    py::class_<biogears::SEScalarPressure,biogears::SEScalarQuantity<biogears::PressureUnit>>(m, "SEScalarPressure")
+//     .def(py::init<>());
 
- 
-#ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
-#else
-    m.attr("__version__") = "dev";
-#endif
+   py::class_<biogears::SEScalarPressure>(m, "SEScalarPressure")
+    .def(py::init<>());
+
 }
