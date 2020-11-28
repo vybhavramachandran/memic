e6e289343e67995a050266003ae34dea3445eaa1
Vybhav <vybhav@hipo.ai>
2020-11-28 12:30:33 +0530

    Before lunch commit. Started working on the SEDataRequestManager binding file

---
+++ projects/biogears/libBiogears/src/pybwrappers/cdm/scenario/requests/pybSEDataRequestManager.cpp
@@ -0,0 +1,28 @@
+#include <biogears/cdm/scenario/requests/SEDataRequestManager.h>
+#include <biogears/cdm/utils/Logger.h>
+
+#include <pybind11/pybind11.h>
+
+
+namespace py = pybind11;
+
+
+
+
+PYBIND11_MODULE(pybSEDataRequestManager, m) {
+
+
+    py::class_<biogears::SEDataRequestManager>(m, "SEDataRequestManager")
+    .def(py::init<biogears::Logger*>())
+    .def("Clear",&biogears::SESubstanceManager::Clear)
+    .def("Reset",&biogears::SESubstanceManager::Reset)
+    .def("LoadSubstanceDirectory",&biogears::SESubstanceManager::LoadSubstanceDirectory);
+
+
+ 
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
