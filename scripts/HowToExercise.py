fde52d4c427463871c9d93f4a3bd7ab9df73db44
Vybhav <vybhav@hipo.ai>
2020-12-12 03:10:55 +0530

    Adding the scripts folder. Created the constants python file

---
+++ scripts/HowToExercise.py
@@ -0,0 +1,34 @@
+import pybLogger
+import pybBioGearsEngine
+import pybBioGearsEngineHeader
+import pybPhysiologyEngineTrack
+import pybSEDataRequest
+import pybSEDataRequestManager
+import pybSEPhysiologyDataRequest
+from src_cdm_properties_constants import *
+
+var_logger = pybLogger.Logger("HowToExercise.log","")
+bg = pybBioGearsEngine.CreateBioGearsEngine(var_logger)
+bg.LoadState("./states/StandardMale@0s.xml")
+
+bg.GetEngineTrack()
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('CardiacOutput',src_cdm_properties_constants["VolumePerTimeUnit::mL_Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('MeanArterialPressure',src_cdm_properties_constants["PressureUnit::mmHg"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('SystolicArterialPressure',src_cdm_properties_constants["PressureUnit::mmHg"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('DiastolicArterialPressure',src_cdm_properties_constants["PressureUnit::mmHg"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('TotalMetabolicRate',src_cdm_properties_constants["PowerUnit::kcal_Per_day"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+bg.GetEngineTrack().GetDataRequestManager().CreatePhysiologyDataRequest().Set('HeartRate',src_cdm_properties_constants["FrequencyUnit::Per_min"])
+
+
+bg.GetEngineTrack().GetDataRequestManager().SetResultsFileName('HowToExercise.csv')
+
+
+
+
+
