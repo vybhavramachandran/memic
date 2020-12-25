b2fac8c6af9e2e7937f35e754fbeb31ce833802b
Vybhav <vybhav@hipo.ai>
2020-12-25 09:09:29 +0530

    Some more commits to the SECompartmentManager. Fixed some errors

--- projects/biogears/libBiogears/src/pybwrappers/cdm/compartment/pybSECompartmentManager.cpp
+++ projects/biogears/libBiogears/src/pybwrappers/cdm/compartment/pybSECompartmentManager.cpp
@@ -8,7 +8,6 @@
 #include <biogears/cdm/CommonDataModel.h>
 #include <biogears/cdm/circuit/SECircuitManager.h>
 #include <biogears/cdm/compartment/SECompartment.h>
-#include <biogears/cdm/compartment/SECompartmentManager.h>
 #include <biogears/cdm/compartment/fluid/SEGasCompartment.h>
 #include <biogears/cdm/compartment/fluid/SEGasCompartmentGraph.h>
 #include <biogears/cdm/compartment/fluid/SELiquidCompartment.h>
@@ -23,6 +22,8 @@
 #include <biogears/schema/cdm/Scenario.hxx>
 #include <pybind11/pybind11.h>
 #include <string>
+#include <biogears/cdm/compartment/SECompartmentManager.h>
+
 
 namespace py = pybind11;
 
@@ -37,16 +38,31 @@ PYBIND11_MODULE(pybSECompartmentManager, m)
     .def("Load", &biogears::SECompartmentManager::Load)
     .def("Unload", py::overload_cast<>(&biogears::SECompartmentManager::Unload, py::const_))
     .def("StateChange", &biogears::SECompartmentManager::StateChange)
-    .def("UpdateLinks", py::overload_cast<>(&biogears::SECompartmentManager::UpdateLinks))
-    .def("UpdateLinks", py::overload_cast<biogears::SEGasCompartmentGraph&>(&biogears::SECompartmentManager::UpdateLinks))
-    .def("UpdateLinks", py::overload_cast<biogears::SELiquidCompartmentGraph&>(&biogears::SECompartmentManager::UpdateLinks))
-    .def("HasCompartment", py::overload_cast<CDM::enumCompartmentType::value, const char*>(&biogears::SECompartmentManager::HasCompartment, py::const_))
-    .def("HasCompartment", py::overload_cast<CDM::enumCompartmentType::value, const std::string&>(&biogears::SECompartmentManager::HasCompartment, py::const_))
-    .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const char*>(&biogears::SECompartmentManager::GetCompartment))
-    .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const std::string&>(&biogears::SECompartmentManager::GetCompartment))
-    .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const char*>(&biogears::SECompartmentManager::GetCompartment, py::const_))
-    .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const std::string&>(&biogears::SECompartmentManager::GetCompartment, py::const_));
+    // .def("UpdateLinks", py::overload_cast<>(&biogears::SECompartmentManager::UpdateLinks))
+    // .def("UpdateLinks", py::overload_cast<biogears::SEGasCompartmentGraph&>(&biogears::SECompartmentManager::UpdateLinks))
+    // .def("UpdateLinks", py::overload_cast<biogears::SELiquidCompartmentGraph&>(&biogears::SECompartmentManager::UpdateLinks))  
+    // .def("HasCompartment", py::overload_cast<CDM::enumCompartmentType::value, const char*>(&biogears::SECompartmentManager::HasCompartment, py::const_))
+    // .def("HasCompartment", py::overload_cast<CDM::enumCompartmentType::value, const std::string&>(&biogears::SECompartmentManager::HasCompartment, py::const_))
+    // .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const char*>(&biogears::SECompartmentManager::GetCompartment))
+    // .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const std::string&>(&biogears::SECompartmentManager::GetCompartment))
+    // .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const char*>(&biogears::SECompartmentManager::GetCompartment, py::const_))
+    // .def("GetCompartment", py::overload_cast<CDM::enumCompartmentType::value, const std::string&>(&biogears::SECompartmentManager::GetCompartment, py::const_));
+    .def("CreateGasCompartment", py::overload_cast<const char*>(&biogears::SECompartmentManager::CreateGasCompartment))
+    .def("CreateGasCompartment", py::overload_cast<const std::string&>(&biogears::SECompartmentManager::CreateGasCompartment))
+    .def("HasGasCompartment", py::overload_cast<const char*>(&biogears::SECompartmentManager::HasGasCompartment,py::const_))
+    .def("HasGasCompartment", py::overload_cast<const std::string&>(&biogears::SECompartmentManager::HasGasCompartment,py::const_))
+    .def("GetGasCompartment", py::overload_cast<const char*>(&biogears::SECompartmentManager::GetGasCompartment))
+    .def("GetGasCompartment", py::overload_cast<const std::string&>(&biogears::SECompartmentManager::GetGasCompartment))
+    .def("GetGasCompartment", py::overload_cast<const char*>(&biogears::SECompartmentManager::GetGasCompartment,py::const_))
+    .def("GetGasCompartment", py::overload_cast<const std::string&>(&biogears::SECompartmentManager::GetGasCompartment,py::const_))
+    .def("GetGasCompartments", &biogears::SECompartmentManager::GetGasCompartments)
+    .def("GetGasLeafCompartments", &biogears::SECompartmentManager::GetGasLeafCompartments)
+    .def("CreateGasLink", py::overload_cast<biogears::SEGasCompartment&, biogears::SEGasCompartment&, const char*>(&biogears::SECompartmentManager::CreateGasLink))
+    .def("CreateGasLink", py::overload_cast<biogears::SEGasCompartment&, biogears::SEGasCompartment&, const std::string&>(&biogears::SECompartmentManager::CreateGasLink))
+    .def("DeleteGasLink", py::overload_cast<const char*>(&biogears::SECompartmentManager::DeleteGasLink))
+    .def("DeleteGasLink", py::overload_cast<const std::string&>(&biogears::SECompartmentManager::DeleteGasLink));
 
+    
 #ifdef VERSION_INFO
   m.attr("__version__")
     = VERSION_INFO;
